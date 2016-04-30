2eae8a53df5bb83c50fcbc6a98e0f2e45242bea3
Happa8 <okadakoutaro@gmail.com>
2016-05-01 02:42:15 +0900

    数値入力まで出来るようになった

--- DataAnalysiser/MainWindow.xaml.cs
+++ DataAnalysiser/MainWindow.xaml.cs
@@ -20,9 +20,53 @@ namespace DataAnalysiser
     /// </summary>
     public partial class MainWindow : Window
     {
+        public double[] data_entry_array = new double[1];
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void numerical_entry_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            bool yes_parse = false;
+            {
+                // 既存のテキストボックス文字列に、
+                // 今新規に一文字追加された時、その文字列が
+                // 数値として意味があるかどうかをチェック
+                {
+                    float xx;
+                    var tmp = numerical_entry.Text + e.Text;
+                    yes_parse = Single.TryParse(tmp, out xx);
+                }
+                // 最初の一文字の場合に限り、プラス文字とマイナス文字の
+                // 入力は許可する。
+                yes_parse |=
+                    (numerical_entry.Text.Length == 0) &&
+                    (e.Text == "+") || (e.Text == "-");
+            }
+            // 更新したい場合は false, 更新したくない場合は true
+            // を返すべし。（混乱しやすいので注意！）
+            e.Handled = !yes_parse;
+            //from(http://d.hatena.ne.jp/nurs/20150427/1430150521)
+        }
+
+        private void numerical_entry_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                //データ入れとく配列の長さを取得し、配列の長さを一個分増やす
+                Array.Resize(ref data_entry_array, data_entry_array.Length + 1);
+                data_entry_array[data_entry_array.Length - 1] = double.Parse(numerical_entry.Text);
+
+                //textboxの中身をリセット
+                numerical_entry.Clear();
+            }
         }
     }
 }
