04dfca51e919e594e96c46171823998d044d3559
Happa8 <okadakoutaro@gmail.com>
2016-06-14 01:59:42 +0900

    [fix]setScaleの計算式に間違いがあったので修正 結局小数点切捨ての関数もMath.Truncateに戻した

--- DataAnalysiser/BoxplotSystem.cs
+++ DataAnalysiser/BoxplotSystem.cs
@@ -20,25 +20,25 @@ namespace DataAnalysiser
             double minScale = 0;
             if (maxValue >= 0 && minValue >= 0)
             {
-                maxScale = (Math.Floor(maxValue / Math.Pow(10, maxDigits - 1)) % 10 + 1) * Math.Pow(10, maxDigits - 1);
+                maxScale = (Math.Truncate(maxValue / Math.Pow(10, maxDigits - 1)) % 10 + 1) * Math.Pow(10, maxDigits - 1);
                 minScale = 0;
             }
-            else if (maxValue >= 0 && minValue < 0 && maxValue >= minValue)
+            else if (maxValue >= 0 && minValue < 0 && Math.Abs(maxValue) >= Math.Abs(minValue))
             {
-                maxScale = (Math.Floor(maxValue / Math.Pow(10, maxDigits - 1)) % 10 + 1) * Math.Pow(10, maxDigits - 1);
+                maxScale = (Math.Truncate(maxValue / Math.Pow(10, maxDigits - 1)) % 10 + 1) * Math.Pow(10, maxDigits - 1);
                 minScale = -1 * maxScale;
             }
-            else if(maxValue >= 0 && minValue < 0 && maxValue < minValue)
+            else if(maxValue >= 0 && minValue < 0 && Math.Abs(maxValue) < Math.Abs(minValue))
             {
                 int minDigits = (-1 * minValueI).ToString().Length;
-                minScale = -1 * (Math.Floor((-1 * minValue) / Math.Pow(10, minDigits - 1)) % 10 - 1) * Math.Pow(10, minDigits - 1);
-                maxScale = -1 * minValue;
+                minScale = (Math.Truncate(minValue / Math.Pow(10, minDigits - 1)) % 10 - 1) * Math.Pow(10, minDigits - 1);
+                maxScale = -1 * minScale;
             }
             else if(maxValue < 0 && minValue < 0)
             {
                 maxScale = 0;
                 int minDigits = (-1 * minValueI).ToString().Length;
-                minScale = -1 * (Math.Floor((-1 * minValue) / Math.Pow(10, minDigits - 1)) % 10 - 1) * Math.Pow(10, minDigits - 1);
+                minScale = (Math.Truncate(minValue / Math.Pow(10, minDigits - 1)) % 10 - 1) * Math.Pow(10, minDigits - 1);
             }
 
             result[0] = minScale;
