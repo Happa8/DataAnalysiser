43d736956fd33a588178c75f94f23f5994457366
Happa8 <okadakoutaro@gmail.com>
2016-06-14 01:45:18 +0900

    [modify]setScaleの小数点切捨て関数をMath.Floorに変更

--- DataAnalysiser/BoxplotSystem.cs
+++ DataAnalysiser/BoxplotSystem.cs
@@ -20,25 +20,25 @@ namespace DataAnalysiser
             double minScale = 0;
             if (maxValue >= 0 && minValue >= 0)
             {
-                maxScale = (Math.Truncate(maxValue / Math.Pow(10, maxDigits - 1)) % 10 + 1) * Math.Pow(10, maxDigits - 1);
+                maxScale = (Math.Floor(maxValue / Math.Pow(10, maxDigits - 1)) % 10 + 1) * Math.Pow(10, maxDigits - 1);
                 minScale = 0;
             }
             else if (maxValue >= 0 && minValue < 0 && maxValue >= minValue)
             {
-                maxScale = (Math.Truncate(maxValue / Math.Pow(10, maxDigits - 1)) % 10 + 1) * Math.Pow(10, maxDigits - 1);
+                maxScale = (Math.Floor(maxValue / Math.Pow(10, maxDigits - 1)) % 10 + 1) * Math.Pow(10, maxDigits - 1);
                 minScale = -1 * maxScale;
             }
             else if(maxValue >= 0 && minValue < 0 && maxValue < minValue)
             {
                 int minDigits = (-1 * minValueI).ToString().Length;
-                minScale = -1 * (Math.Truncate((-1 * minValue) / Math.Pow(10, minDigits - 1)) % 10 - 1) * Math.Pow(10, minDigits - 1);
+                minScale = -1 * (Math.Floor((-1 * minValue) / Math.Pow(10, minDigits - 1)) % 10 - 1) * Math.Pow(10, minDigits - 1);
                 maxScale = -1 * minValue;
             }
             else if(maxValue < 0 && minValue < 0)
             {
                 maxScale = 0;
                 int minDigits = (-1 * minValueI).ToString().Length;
-                minScale = -1 * (Math.Truncate((-1 * minValue) / Math.Pow(10, minDigits - 1)) % 10 - 1) * Math.Pow(10, minDigits - 1);
+                minScale = -1 * (Math.Floor((-1 * minValue) / Math.Pow(10, minDigits - 1)) % 10 - 1) * Math.Pow(10, minDigits - 1);
             }
 
             result[0] = minScale;
